730f8dbc67c198f13bf4558570adc62cb46a2a11
Sanidhya Vijaivargia <53777743+Sanidhya0077@users.noreply.github.com>
2021-10-22 17:08:09 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-
-async function display() {
-  let num = document.getElementById("pgnum").innerHTML;
+const allbtns = document.querySelectorAll("#pgnum")
+allbtns.forEach(btn=>{
+  btn.addEventListener("click",(e)=> display(e))
+})
+async function display(e) {
+  const num = parseInt(e.target.innerHTML)
   const api_url = `https://api.github.com/repos/octocat/hello-world/issues?page=${num}`;
   const rawData = await fetch(api_url);
   const parsedData = await rawData.json();
-  console.log(parsedData);
   let tab = `<tr>
       <th>Issues</th>    
     </tr>`;
-
   for (let i = 0; i < parsedData.length; i++) {
     tab += `<tr>
         <td> ${parsedData[i].title}</td>
@@ -18,4 +18,3 @@ async function display() {
   }
   document.getElementById("rep").innerHTML = tab;
 }
-
